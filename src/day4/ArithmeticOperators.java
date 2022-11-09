4dfb9a5655767b20fdbb41649987816410878af5
Sundeep9913 <sundeep_jg@outlook.com>
2022-11-08 21:19:23 -0500

    created new class ArithmeticOperators

---
+++ src/day4/ArithmeticOperators.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args){
+        int a = 6;
+        int b = 5;
+
+        add (a,b);
+    }
+
+    private static void add(int a, int b) {
+        float c = a / b;
+
+        System.out.println(c);
+    }
+
+}
